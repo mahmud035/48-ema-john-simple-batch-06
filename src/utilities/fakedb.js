f606e1839dfc8e8280183a61467861469a05bcca
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-25 23:24:02 +0600

    Load cart from local storage, find product

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,12 +1,6 @@
 //* use local storage to manage cart data
 const addToDb = (id) => {
-  let shoppingCart = {};
-
-  //* get the shopping cart from local storage
-  const storedCart = localStorage.getItem('shopping-cart');
-  if (storedCart) {
-    shoppingCart = JSON.parse(storedCart);
-  }
+  const shoppingCart = getStoredCart();
 
   //* add quantity
   const quantity = shoppingCart[id];
@@ -17,6 +11,17 @@ const addToDb = (id) => {
     shoppingCart[id] = 1;
   }
   localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+};
+
+const getStoredCart = () => {
+  let shoppingCart = {};
+
+  //* get the shopping cart from local storage
+  const storedCart = localStorage.getItem('shopping-cart');
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  }
+  return shoppingCart;
 };
 
 const removeFromDb = (id) => {
@@ -34,4 +39,4 @@ const deleteShoppingCart = () => {
   localStorage.removeItem('shopping-cart');
 };
 
-export { addToDb, removeFromDb, deleteShoppingCart };
+export { addToDb, removeFromDb, deleteShoppingCart, getStoredCart };
