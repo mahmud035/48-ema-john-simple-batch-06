892139b428d0209543f1e98b19dd7ec4f2fd3050
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-06 22:08:02 +0600

    added error page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 import Inventory from './Components/Inventory/Inventory';
 import Orders from './Components/Orders/Orders';
 import Shop from './Components/Shop/Shop';
@@ -11,6 +12,7 @@ function App() {
     {
       path: '/',
       element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
           path: '/',
