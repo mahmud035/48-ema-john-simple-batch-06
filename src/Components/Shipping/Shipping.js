5641b275f4ad8c8f6b46a51818298ac2326b5226
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-19 11:38:46 +0600

    added redirects file

--- src/Components/Shipping/Shipping.js
+++ src/Components/Shipping/Shipping.js
@@ -5,6 +5,10 @@ const Shipping = () => {
   return (
     <div>
       <h2>Shipping</h2>
+      <p>Delivery on the way!!!</p>
+      <p>
+        <small>Coming Soon....</small>
+      </p>
     </div>
   );
 };
